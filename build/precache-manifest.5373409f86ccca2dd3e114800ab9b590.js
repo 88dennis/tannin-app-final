a058cfca44a52173627b946f45534ee7610e7365
Dennis <dennissarmiento808@gmail.com>
2019-06-22 17:30:57 +0800

    up

---
+++ build/precache-manifest.5373409f86ccca2dd3e114800ab9b590.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e1a31a0820914fb47b60d03788cd4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ab11eeb09dacf9b6072",
+    "url": "/static/css/main.75381a87.chunk.css"
+  },
+  {
+    "revision": "877d7e8a1262f97ce3a8",
+    "url": "/static/js/2.5ba5e0b2.chunk.js"
+  },
+  {
+    "revision": "6ab11eeb09dacf9b6072",
+    "url": "/static/js/main.862a708d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
