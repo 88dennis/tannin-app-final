1de5e17f301e82222384869a016f382b1e841d1b
Dennis <dennissarmiento808@gmail.com>
2020-06-26 15:00:41 -0400

    up

---
+++ build/precache-manifest.67bd5371a92585ed01a84aba09b33f9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf19fb557cff661aa86bc2ff232836b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15ad2894cdd61cfd166f",
+    "url": "/static/css/main.df83bedb.chunk.css"
+  },
+  {
+    "revision": "eb3575b1fc9e7abefc25",
+    "url": "/static/js/2.2d92211e.chunk.js"
+  },
+  {
+    "revision": "69efa89445bca9e61047cd45f288f95b",
+    "url": "/static/js/2.2d92211e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ad2894cdd61cfd166f",
+    "url": "/static/js/main.f008a00d.chunk.js"
+  },
+  {
+    "revision": "bac437e66c95eed8021a",
+    "url": "/static/js/runtime-main.abaa33c9.js"
+  }
+]);
