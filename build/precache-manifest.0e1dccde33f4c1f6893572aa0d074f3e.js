df0b52ef0067bda2a036ef89954f96e072dfd872
Dennis <dennissarmiento808@gmail.com>
2020-06-26 11:41:55 -0400

    up

---
+++ build/precache-manifest.0e1dccde33f4c1f6893572aa0d074f3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f193830a3bb9467f35142dbf1f3909a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9a9446b1b8cf419b684",
+    "url": "/static/css/main.f8d66987.chunk.css"
+  },
+  {
+    "revision": "eb3575b1fc9e7abefc25",
+    "url": "/static/js/2.2d92211e.chunk.js"
+  },
+  {
+    "revision": "69efa89445bca9e61047cd45f288f95b",
+    "url": "/static/js/2.2d92211e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a9446b1b8cf419b684",
+    "url": "/static/js/main.db51610a.chunk.js"
+  },
+  {
+    "revision": "bac437e66c95eed8021a",
+    "url": "/static/js/runtime-main.abaa33c9.js"
+  }
+]);
